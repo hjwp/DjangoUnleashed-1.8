f5bca7924ed4b08d459187a4a77fd9268430d53a
Andrew Pinkham <code@andrewsforge.com>
2014-01-09 17:49:03 -0600

    Ch09: Create Tag Create view.

--- organizer/views.py
+++ organizer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import (
-    get_object_or_404, render)
+    get_object_or_404, redirect, render)
 
+from .forms import TagForm
 from .models import Startup, Tag
 
 
@@ -18,6 +19,34 @@ def startup_list(request):
         request,
         'organizer/startup_list.html',
         {'startup_list': Startup.objects.all()})
+
+
+def tag_create(request):
+    # If the form has been submitted...
+    if request.method == 'POST':
+        # Bind the form to the POST data
+        form = TagForm(request.POST)
+        # If validation rules pass
+        if form.is_valid():
+            # Create new tag object
+            new_tag = form.save()
+            # Redirect to new Tag
+            # uses Tag.get_absolute_url
+            return redirect(new_tag)
+        # implicit else:
+        #   bound form (with errors)
+        #   passed back to template (line 45)
+    # if request.method != POST
+    else:
+        # create an unbound form instance
+        form = TagForm()
+    # return either:
+    #     (1) the bound+invalid form on line 27
+    #     (2) the unbound form from line 41
+    return render(
+        request,
+        'organizer/tag_form.html',
+        {'form': form})
 
 
 def tag_detail(request, slug):
