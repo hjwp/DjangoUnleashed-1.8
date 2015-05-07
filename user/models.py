f397d5f97c132d7da66f1265f4dfb45242b10edd
Andrew Pinkham <code@andrewsforge.com>
2015-05-06 20:41:39 -0500

    Ch20: Create Custom User Model.

--- user/models.py
+++ user/models.py
@@ -1,6 +1,10 @@
 from django.conf import settings
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager,
+    PermissionsMixin)
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils.text import slugify
 
 
 class Profile(models.Model):
@@ -24,3 +28,34 @@ class Profile(models.Model):
         return reverse(
             'dj-auth:public_profile',
             kwargs={'slug': self.slug})
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        'email address',
+        max_length=254,
+        unique=True)
+    is_staff = models.BooleanField(
+        'staff status',
+        default=False,
+        help_text=(
+            'Designates whether the user can '
+            'log into this admin site.'))
+    is_active = models.BooleanField(
+        'active',
+        default=True,
+        help_text=(
+            'Designates whether this user should '
+            'be treated as active. Unselect this '
+            'instead of deleting accounts.'))
+
+    USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = ['name']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def get_absolute_url(self):
+        return self.profile.get_public_url()
